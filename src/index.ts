fed936d9861c5e4a91dacb01d36db80ec43653a4
Justin Jones <justinjones53@gmail.com>
2022-08-29 15:36:58 -0700

    adding rootPackage and dependencyDepth options

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
 import { getPackages } from '@lerna/project';
+import { Package } from '@lerna/package'
 import fs from 'fs';
 import graphviz from 'graphviz';
 import yargs from 'yargs';
+
+type Nodes = Record<string, Node>
+type Edges = Record<string, graphviz.Edge>
+type Node = {
+	node: graphviz.Node
+	edges: Edges
+}
 
 const argv = yargs
 	.help()
@@ -32,7 +40,19 @@ const argv = yargs
 			description: 'File to write into. If not given, outputs on stdout.',
 			type: 'string',
 		},
+		rootPackage: {
+			alias: 'p',
+			description: 'Root package to start drawing map from. If not given, all packages are used.',
+			type: 'string',
+		},
+		dependencyDepth: {
+			alias: 'D',
+			description: 'Depth to traverse dependencies.',
+			type: 'number',
+			default: 3,
+		}
 	}).argv;
+
 
 getPackages().then((packages) => {
 	const g = graphviz.digraph('G');
@@ -43,39 +63,52 @@ getPackages().then((packages) => {
 		g.setGraphVizPath(argv.graphvizDirectory);
 	}
 
-	packages.forEach((pkg) => {
-		const node = g.addNode(pkg.name);
+	const nodes: Nodes = {}
+	const addNode = (name: string): graphviz.Node => {
+		if (nodes[name]) return nodes[name].node
+		const newNode = g.addNode(name)
+		nodes[name] = { node: newNode, edges: {} }
+		return newNode
+	}
+	const addEdge = (a: graphviz.Node, b: string) => {
+		const existingEdge = nodes[a.id].edges[b] 
+		if (existingEdge) return existingEdge
+		const newEdge = g.addEdge(a, b)
+		nodes[a.id].edges[b] = newEdge
+		return newEdge
+	}
 
-		if (pkg.private) {
-			node.set('style', 'dashed');
+	const getPackage = (name : string) => {
+		return packages.find(pkg => {
+			return pkg.name === name
+		})
+	}
+
+	const renderDepsRecursively = ((pkg: Package, depth = 0) => {
+		if (depth >= argv.dependencyDepth) return
+
+		const node = addNode(pkg.name)
+		if (depth === 0) {
+			node.set('style', 'filled')
 		}
+		// console.log('recurse', pkg.name, depth)
 
 		if (pkg.dependencies) {
 			Object.keys(pkg.dependencies).forEach((depName) => {
-				if (packages.find((p) => p.name === depName)) {
-					g.addEdge(node, depName);
+				const p = getPackage(depName)
+				if (p) {
+					addEdge(node, depName)
+					renderDepsRecursively(p, depth + 1)
 				}
 			});
 		}
+	})
 
-		if (pkg.devDependencies) {
-			Object.keys(pkg.devDependencies).forEach((depName) => {
-				if (packages.find((p) => p.name === depName)) {
-					const edge = g.addEdge(node, depName);
-					edge.set('style', 'dashed');
-				}
-			});
-		}
-
-		if (pkg.peerDependencies) {
-			Object.keys(pkg.peerDependencies).forEach((depName) => {
-				if (packages.find((p) => p.name === depName)) {
-					const edge = g.addEdge(node, depName);
-					edge.set('style', 'dotted');
-				}
-			});
-		}
-	});
+	packages
+		.filter(pkg => !argv.rootPackage || argv.rootPackage === pkg.name)
+		.forEach((pkg) => {
+			renderDepsRecursively(pkg, 0)
+		});
 
 	if (argv.outputFormat) {
 		if (argv.outputPath) {
